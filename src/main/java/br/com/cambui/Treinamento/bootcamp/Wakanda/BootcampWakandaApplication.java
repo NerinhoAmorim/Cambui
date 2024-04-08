bd2f5eb173ec5821ba70ce34195b3621fc2ba5b4
Nerinho Nascimento <neramornas@gmail.com>
2024-04-08 10:42:20 -0300

    Inicio projeto

---
+++ src/main/java/br/com/cambui/Treinamento/bootcamp/Wakanda/BootcampWakandaApplication.java
@@ -0,0 +1,13 @@
+package br.com.cambui.Treinamento.bootcamp.Wakanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampWakandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampWakandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cambui/Treinamento/bootcamp/Wakanda/BootcampWakandaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cambui.Treinamento.bootcamp.Wakanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampWakandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
